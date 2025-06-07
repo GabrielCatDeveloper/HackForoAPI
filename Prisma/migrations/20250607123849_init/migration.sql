c91f175ce3eadfbfa2c5317330e8a1617a224410
Gabriel Cortés Garcia <gabriel.cat.developer@gmail.com>
2025-06-07 14:41:24 +0200

    fix table names
    5 min

--- Prisma/migrations/20250607123849_init/migration.sql
+++ Prisma/migrations/20250607123849_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "Campaign" (
+CREATE TABLE "campaigns" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "accessToken" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -16,11 +16,11 @@ CREATE TABLE "users" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "deletedAt" DATETIME,
     "campaignId" INTEGER NOT NULL,
-    CONSTRAINT "users_campaignId_fkey" FOREIGN KEY ("campaignId") REFERENCES "Campaign" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "users_campaignId_fkey" FOREIGN KEY ("campaignId") REFERENCES "campaigns" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "Topic" (
+CREATE TABLE "topics" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "canAdd" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -28,12 +28,12 @@ CREATE TABLE "Topic" (
     "userId" TEXT NOT NULL,
     "campaignId" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
-    CONSTRAINT "Topic_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "Topic_campaignId_fkey" FOREIGN KEY ("campaignId") REFERENCES "Campaign" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "topics_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "topics_campaignId_fkey" FOREIGN KEY ("campaignId") REFERENCES "campaigns" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "Reply" (
+CREATE TABLE "replies" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "topicId" INTEGER NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -43,24 +43,24 @@ CREATE TABLE "Reply" (
     "title" TEXT,
     "message" TEXT NOT NULL,
     "parentId" INTEGER,
-    CONSTRAINT "Reply_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "Topic" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "Reply_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "Reply_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Reply" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+    CONSTRAINT "replies_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "topics" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "replies_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "replies_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "replies" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "ReplyView" (
+CREATE TABLE "replyViews" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "replyUpdatedAt" DATETIME NOT NULL,
     "userId" TEXT NOT NULL,
     "replyId" INTEGER NOT NULL,
-    CONSTRAINT "ReplyView_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "ReplyView_replyId_fkey" FOREIGN KEY ("replyId") REFERENCES "Reply" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "replyViews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "replyViews_replyId_fkey" FOREIGN KEY ("replyId") REFERENCES "replies" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "Message" (
+CREATE TABLE "messages" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "userFromId" TEXT NOT NULL,
     "userToId" TEXT NOT NULL,
@@ -68,9 +68,9 @@ CREATE TABLE "Message" (
     "viewedAt" DATETIME,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "deletedAt" DATETIME,
-    CONSTRAINT "Message_userFromId_fkey" FOREIGN KEY ("userFromId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "Message_userToId_fkey" FOREIGN KEY ("userToId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "messages_userFromId_fkey" FOREIGN KEY ("userFromId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "messages_userToId_fkey" FOREIGN KEY ("userToId") REFERENCES "users" ("nickname") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ReplyView_userId_replyId_replyUpdatedAt_key" ON "ReplyView"("userId", "replyId", "replyUpdatedAt");
+CREATE UNIQUE INDEX "replyViews_userId_replyId_replyUpdatedAt_key" ON "replyViews"("userId", "replyId", "replyUpdatedAt");
